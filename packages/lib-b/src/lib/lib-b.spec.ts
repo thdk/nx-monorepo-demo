f6d34d14a8b298a034a679f3d9c81a627178ae7b
Thomas Dekiere <t.dekiere@gmail.com>
2026-04-07 16:24:31 +0200

    test: fix tests so they work with random values returned by lib

--- packages/lib-b/src/lib/lib-b.spec.ts
+++ packages/lib-b/src/lib/lib-b.spec.ts
@@ -2,6 +2,6 @@ import { libB } from './lib-b.js';
 
 describe('libB', () => {
   it('should work', () => {
-    expect(libB()).toEqual('lib-b');
+    expect(libB()).toMatch(/^lib-b-[\w-]+$/);
   });
 });
